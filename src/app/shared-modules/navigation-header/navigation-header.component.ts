8619c1590bc2a7abb9a623c239efd7876629ee95
yashtank09 <yashtank09@gmail.com>
2023-12-30 09:32:54 +0530

    Mock navigation header with responsive design

--- src/app/shared-modules/navigation-header/navigation-header.component.ts
+++ src/app/shared-modules/navigation-header/navigation-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'yt-navigation-header',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-header.component.css']
 })
 export class NavigationHeaderComponent {
-
+  
 }
