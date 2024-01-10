33afe27fb174bbb62407a711869c798aaf1ec0ca
yashtank09 <yashtank09@gmail.com>
2024-01-11 00:35:18 +0530

    ready with base footer

--- src/app/shared-modules/footer/footer.component.ts
+++ src/app/shared-modules/footer/footer.component.ts
@@ -1,10 +1,18 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'yt-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  providers: [DatePipe]
 })
 export class FooterComponent {
 
+  date: Date = new Date();
+  copyRightYear: number;
+  constructor(){
+    this.copyRightYear = this.date.getUTCFullYear();
+    console.log(this.copyRightYear);
+  }
 }
