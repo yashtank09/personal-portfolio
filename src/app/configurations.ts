6dfb0c6bfd8275f0d44e747aa863e2cced5cc884
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2024-12-04 18:23:22 +0000

    Added New resume download link

--- src/app/configurations.ts
+++ src/app/configurations.ts
@@ -1,3 +1,3 @@
-export const RESUMEDOWNLOADABLE = "https://drive.usercontent.google.com/u/0/uc?id=1EyKQlG6-2Y1JshX-TCe6_g38TBYYwZJs&export=download";
+export const RESUMEDOWNLOADABLE = "https://drive.google.com/file/d/1IqegyaqxvTpr5yfTUWlTO7ekKHKc2uRn/view?usp=sharing";
 export const EMAILAPI = "https://api.mailgun.net/v3/yashtank.co.in/messages";
 export const API_KEY = "";
