8b967f4a902788bddf8922d11d357bb392a58ebe
yashtank09 <yashtank09@gmail.com>
2026-09-01 17:25:38 +0530

    feat(portfolio): update resume link

--- src/app/configurations.ts
+++ src/app/configurations.ts
@@ -1,3 +1,3 @@
-export const RESUMEDOWNLOADABLE = "https://drive.google.com/uc?export=download&id=1ycbzZI0_3ptgPkmVlZlcdKrhM_j2BVuS";
+export const RESUMEDOWNLOADABLE = "https://drive.google.com/file/d/1vy_9v0AkaTc9_SrSM1HGBhd8MeHr2R9R/view?usp=sharing";
 export const EMAILAPI = "https://api.mailgun.net/v3/yashtank.co.in/messages";
 export const API_KEY = "";
